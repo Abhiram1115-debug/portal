0322bf5a8824e5d76ab6687d00b63bc2962218d6
Abhiram1115-debug <abhiramsrikrishna@gmail.com>
2025-04-28 21:04:49 +0530

    models

---
+++ backend/models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  imageUrl: { type: String, required: true },
+  description: { type: String, required: true },
+  contactDetails: {
+    email: { type: String, required: true },
+    phone: { type: String }
+  },
+  category: { type: String, enum: ['lost', 'found'], required: true },
+  status: { type: String, enum: ['pending', 'approved', 'claimed'], default: 'pending' },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Item', itemSchema);
+                
